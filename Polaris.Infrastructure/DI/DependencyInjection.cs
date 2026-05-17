9fb427671c8a227f9a25ef1e844482d02f724eeb
Benjamin <benjamingebauer90@gmail.com>
2026-05-17 10:19:35 +0200

    Start with DI

---
+++ Polaris.Infrastructure/DI/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Polaris.Core.Interfaces;
+using Polaris.Infrastructure.Settings;
+
+namespace Polaris.Infrastructure.DI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddSettings(this IServiceCollection services)
+    {
+        services.AddSingleton<ISettingsService, JsonSettingsService>();
+        
+        return services;
+    }
+
+    public static IServiceCollection AddImmichApi(this IServiceCollection services)
+    {
+        // services.AddHttpClient("ImmichApi");
+        // services.AddSingleton<ImmichApiClient>(...);
+        // services.AddTransient<IAssetRepository, AssetRepository>();
+        // services.AddTransient<IAlbumRepository, AlbumRepository>();
+        
+        return services;
+    }
+}
---
+++ Polaris.Ui/DI/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Polaris.Ui.DI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddViewModels(this IServiceCollection services)
+    {
+        return services;
+    }
+    
+    public static IServiceCollection AddNavigation(this IServiceCollection services)
+    {
+        return services;
+    }
+}
